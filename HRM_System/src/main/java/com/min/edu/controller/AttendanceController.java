38a13f69d4870001b24dd90b9edda2c17dfe0ee2
SeonJiYoon <sjyun05@gmail.com>
2025-03-06 15:57:07 +0900

    feet : attendance.jsp / homeList.jsp 수정

--- HRM_System/src/main/java/com/min/edu/controller/AttendanceController.java
+++ HRM_System/src/main/java/com/min/edu/controller/AttendanceController.java
@@ -1,10 +1,5 @@
 package com.min.edu.controller;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,11 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.min.edu.dto.AttendanceDto;
 import com.min.edu.dto.EmployeeDto;
-import com.min.edu.dto.VacationDto;
 import com.min.edu.model.service.IAttendanceService;
 import com.min.edu.model.service.IVacationService;
 
@@ -35,7 +28,7 @@ public class AttendanceController {
 	private final IAttendanceService attendanceService;
 	private final IVacationService vacationService;
 	
-	@GetMapping(value = "/attendanceListByEmpId")
+	@GetMapping(value = "/attendance")
 	public String attendanceListByEmpId(HttpSession session, Model model) {
 		EmployeeDto loginVo = (EmployeeDto)session.getAttribute("loginVo");
 		
@@ -46,216 +39,44 @@ public class AttendanceController {
 		
 		String empId = loginVo.getEmp_id();
 		List<AttendanceDto> lists = attendanceService.attendanceListByEmpId(empId);
-		
-		// 보상시간 조회
-		int extraTime = vacationService.selectExtraTime(empId);
-		
 		model.addAttribute("lists", lists);
-		model.addAttribute("extraTime", extraTime); // 조회된 보상시간 값
-		
-		return "attendanceListByEmpId";
+
+		return "attendance";
 	}
 	
 	@PostMapping(value = "/insertAttendance")
-	public String insertAttendance(HttpSession session) {
+	public String insertAttendance(HttpSession session, Model model) {
 		log.info("출근 버튼 클릭");
 		
 		EmployeeDto loginVo = (EmployeeDto)session.getAttribute("loginVo");
-		
 		if (loginVo == null) {
 			log.info("로그인 정보 없음.");
 			return "redirect:/loginForm";
 		}
 		
 		String empId = loginVo.getEmp_id();
+		attendanceService.insertAttendanceLogic(empId);
 		
-		// 출근시간 입력
-		int result = attendanceService.insertAttendance(empId);
-		
-		if(result > 0) {
-			log.info("출근 기록 저장 완료");
-		} else {
-			log.info("출근 기록 저장 실패");
-		}
-		
-		// 방금 저장한 출근시간 가져오기
-		String checkInDateStr = attendanceService.selectClockIn(empId);
-		
-		// 기준 출근 시간 (09시)
-		Calendar cal = new GregorianCalendar();
-		String checkOutDateStr = String.format("%d%02d%02d090000",
-			cal.get(Calendar.YEAR),
-			cal.get(Calendar.MONTH) + 1,
-			cal.get(Calendar.DATE)
-		);
-		
-		// Date로 변환할 Format
-		SimpleDateFormat dbFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		SimpleDateFormat customFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-		
-		try {
-			Date checkInDate = dbFormat.parse(checkInDateStr);
-			Date checkOutDate = customFormat.parse(checkOutDateStr);
-			
-			if(checkInDate.after(checkOutDate)) { // 지각
-				Map<String, Object> AttendtypeMap = new HashMap<>();
-				AttendtypeMap.put("attendType", "지각");
-				AttendtypeMap.put("empId", empId);
-				
-				attendanceService.updateAttendtype(AttendtypeMap);
-			}
-			
-		} catch (ParseException e) {
-			log.error("날짜 변환 오류", e);
-		}
-		
-		return "redirect:attendanceListByEmpId";
+		return "redirect:/homeList.do";
 	}
 	
 	@PostMapping(value = "/updateAttendance")
-	public ResponseEntity<Map<String, Object>> updateAttendance(HttpSession session,
-															    @RequestBody Map<String, String> infoAtten) {
+	public ResponseEntity<Map<String, Object>> updateAttendance(HttpSession session, @RequestBody Map<String, String> infoAtten) {
 		log.info("퇴근 버튼 클릭");
-		log.info("퇴근 정보 확인 {}", infoAtten);
 		
 		EmployeeDto loginVo = (EmployeeDto)session.getAttribute("loginVo");
+		if (loginVo == null) {
+			log.info("로그인 정보 없음.");
+			return ResponseEntity.badRequest().build();
+		}
+		
 		String empId = loginVo.getEmp_id();
+		attendanceService.updateAttendanceLogic(empId, infoAtten);
 		
-		// infoAtten 에서 exitHour와 useBonusTime 값을 가져오기
-		String exitHour = infoAtten.get("exitHour");
-		String useBonusTime = infoAtten.get("useBonusTime");
-		
-		// 퇴근시간 저장 (현재시간)
-		int result = attendanceService.updateAttendance(empId);
-		if(result > 0) {
-			log.info("퇴근 기록 저장 완료");
-		} else {
-			log.info("퇴근 기록 저장 실패");
-		}
-		
-		// 방금 저장한 퇴근 시간 가져오기
-		String checkInDateStr = attendanceService.selectClockOut(empId);
-		
-		// 근무형태 가져오기 (지각인지 판단여부)
-		String checkLate = attendanceService.selectAttendtype(empId);
-		
-		if(checkLate == null) {
-			if(exitHour == "N") {
-				if(useBonusTime == "Y") { // 보상시간 사용하여 퇴근
-					calAttendance(empId);
-					calculateExtraTime(empId, checkInDateStr);
-				} else { // 조퇴
-					calAttendance(empId);
-					
-					Map<String, Object> attendtypeMap = new HashMap<>();
-					attendtypeMap.put("attendType", "조퇴");
-					attendtypeMap.put("empId", empId);
-					
-					attendanceService.updateAttendtype(attendtypeMap);
-				}
-			} else { // 정상퇴근
-				calAttendance(empId);
-				calculateExtraTime(empId, checkInDateStr);
-			}
-		} else {
-			calAttendance(empId);
-		}
-		
-		Map<String, Object> response = new HashMap<String, Object>();
+		Map<String, Object> response = new HashMap<>();
 		response.put("isc", Boolean.TRUE);
 		
 		return ResponseEntity.ok(response);
 	}
 	
-	// 근무시간 계산 메소드
-	private void calAttendance(String empId) {
-		int result = attendanceService.calAttendance(empId);
-		
-		if(result > 0) {
-			log.info("근무 시간 계산 완료");
-		} else {
-			log.info("근무 시간 계산 실패");
-		}
-	}
-	
-	// 보상시간 계산 메소드
-	private void calculateExtraTime(String empId, String checkInDateStr) {
-		log.info("보상시간 계산 시작");
-		
-		// 기준 퇴근 시간 (18시)
-		Calendar cal = new GregorianCalendar();
-		String checkOutDateStr = String.format("%d%02d%02d180000",
-			cal.get(Calendar.YEAR),
-			cal.get(Calendar.MONTH) + 1,
-			cal.get(Calendar.DATE)
-		);
-		
-		// Date로 변환할 Format
-		SimpleDateFormat dbFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		SimpleDateFormat customFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-		
-		try {
-			Date checkInDate = dbFormat.parse(checkInDateStr);
-			Date checkOutDate = customFormat.parse(checkOutDateStr);
-			
-			// 차이값을 시간 단위로 변경
-			int diffHour = (int)(checkInDate.getTime() - checkOutDate.getTime())/(60*60*1000); // 60초*60분*밀리세컨드
-			log.info("퇴근 시간 차이 : {}", diffHour);
-			
-			// 보상시간 로직
-			if(checkInDate.after(checkOutDate)) { // 야근 및 정상근무
-				Map<String, Object> ExtraTimeMap = new HashMap<>();
-				ExtraTimeMap.put("extraTime", diffHour);
-				ExtraTimeMap.put("empId", empId);
-				
-				Map<String, Object> AttendtypeMap = new HashMap<>();
-				AttendtypeMap.put("attendType", "정상근무");
-				AttendtypeMap.put("empId", empId);
-				
-				vacationService.updateExtraTime(ExtraTimeMap);
-				attendanceService.updateAttendtype(AttendtypeMap);
-			} else { // 18시보다 일찍 퇴근했을 때
-				
-				diffHour--;
-				int bonus = vacationService.selectExtraTime(empId);
-				log.info("보유한 보상시간 : {}", bonus);
-				
-				if((-1)*diffHour <= bonus) { // 보상시간을 사용할 수 있을 때
-					log.info("사용한 보상시간 : {}", diffHour);
-					
-					Map<String, Object> extraTimeMap = new HashMap<>();
-					extraTimeMap.put("empId", empId);
-					extraTimeMap.put("extraTime", diffHour);
-					
-					vacationService.updateExtraTime(extraTimeMap);
-					
-					Map<String, Object> useExtraTimeMap = new HashMap<>();
-					useExtraTimeMap.put("empId", empId);
-					useExtraTimeMap.put("useExtraTime", (-1)*diffHour);
-					
-					attendanceService.updateUseExtraTime(useExtraTimeMap);
-					
-					int useBonus = vacationService.selectExtraTime(empId);
-					log.info("남은 보상시간 : {}", useBonus);
-					
-					Map<String, Object> attendtypeMap = new HashMap<>();
-					attendtypeMap.put("attendType", "정상근무");
-					attendtypeMap.put("empId", empId);
-					
-					attendanceService.updateAttendtype(attendtypeMap);
-				} else {
-					log.info("보유한 보상시간 부족, 조퇴처리");
-					
-					Map<String, Object> attendtypeMap = new HashMap<>();
-					attendtypeMap.put("attendType", "조퇴");
-					attendtypeMap.put("empId", empId);
-					
-					attendanceService.updateAttendtype(attendtypeMap);
-				}
-			}
-			
-		} catch (ParseException e) {
-			log.error("날짜 변환 오류", e);
-		}
-	}
 }
--- HRM_System/src/main/webapp/WEB-INF/views/homeList.jsp
+++ HRM_System/src/main/webapp/WEB-INF/views/homeList.jsp
@@ -6,16 +6,92 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>홈페이지</title>
-<script type="text/javascript" src="./js/reservation.js"></script>
-
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+	<meta charset="UTF-8">
+	<title>홈페이지</title>
+	<style>
+		.btn.btn-light-primary {
+			color: var(--bs-primary);
+			border-color: var(--bs-primary-light);
+			background-color: var(--bs-primary-light);
+		}
+		
+		.btn.btn-light-primary:hover {
+			color: var(--bs-primary-white);
+			border-color: var(--bs-primary);
+			background-color: var(--bs-primary);
+		}
+		
+		.btn.btn-light-primary:focus {
+			outline: none;
+			box-shadow: 0 0 0 0.25rem rgba(var(--bs-primary), 0.5);
+		}
+		
+		.btn.btn-light-secondary {
+			color: var(--bs-secondary);
+			border-color: var(--bs-secondary-light);
+			background-color: var(--bs-secondary-light);
+		}
+		
+		.btn.btn-light-secondary:hover {
+			color: var(--bs-white);
+			border-color: var(--bs-secondary);
+			background-color: var(--bs-secondary);
+		}
+		
+		.btn.btn-light-secondary:focus {
+			outline: none;
+			box-shadow: 0 0 0 0.25rem rgba(var(--bs-secondary), 0.5);
+		}
+		.main-content {
+			  position: relative;
+			  width: 100%;
+			  padding-top: 20px; /* 위쪽 여백 추가 */
+			  text-align: center; /* 버튼 가운데 정렬 */
+		}
+		.button-container {
+		  display: flex;
+		  gap: 20px; /* 버튼 간격 */
+		  margin-bottom: 20px; /* 아래쪽 여백 */
+		}
+		.button-container button {
+		  flex: 1; /* 버튼 크기를 동일하게 확장 */
+ 		  max-width: 300px; /* 버튼 최대 너비 설정 */
+		  height: 60px; /* 버튼 높이 */
+		  font-size: 18px; /* 글자 크기 */
+		  font-weight: bold; /* 글자 굵게 */
+		}
+	</style>
+<script type="text/javascript" src="./js/attendance.js"></script>
 </head>
 <%@ include file="sidebar.jsp"%>
 <body>
 	<div class="content" id="content">
 		<%@ include file="header.jsp"%>
-		<div class="main-content"></div>
+		<div>
+			<input type="hidden" id="extraTime" value="${extraTime}">
+		</div>
+		<div class="main-content">
+			<div class="button-container">
+				<form id="clockInForm" action="/insertAttendance" method="post">
+					<button type="submit" class="btn btn-light-primary" id="clockIn"
+					    <c:if test="${isClockedIn}">disabled</c:if>>출근
+					</button>
+				</form>
+				<form id="clockOutForm">
+					<button type="button" class="btn btn-light-secondary" id="clockOut" 
+						<c:if test="${!isClockedIn}">disabled</c:if>>퇴근
+					</button>
+				</form>
+				<div>
+				출근 시간 : <span id="clockInTime">${clockIn}</span><br>
+				퇴근 시간 : <span id="clockOutTime">${clockOut}</span>
+				</div> 
+			</div>
+		</div>
 	</div>
+
+
 </body>
 </html>
